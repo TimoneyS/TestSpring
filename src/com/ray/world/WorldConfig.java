59c7406b8791b7828a2f251f664541568a67d46e
Ray <Rays1990@163.com>
2018-03-20 15:12:57 +0800

    build a world

--- src/com/ray/world/WorldConfig.java
+++ src/com/ray/world/WorldConfig.java
@@ -1,10 +1,20 @@
 package com.ray.world;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import com.ray.world.manager.CatManager;
 
 @Configuration
+@EnableAspectJAutoProxy
 @ComponentScan()
 public class WorldConfig {
-
+    
+    @Bean
+    public CatManager catManager() {
+        return new CatManager();
+    }
+    
 }
--- src/com/ray/world/manager/CatManager.java
+++ src/com/ray/world/manager/CatManager.java
@@ -1,8 +1,30 @@
 package com.ray.world.manager;
 
-import org.springframework.stereotype.Component;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
-@Component
+import com.ray.util.io.Out;
+
+@Aspect
 public class CatManager {
-
+    
+    public int catNum;
+    
+    @Before("execution(** com.ray.world.factory.alive.animal.CatFactory.create(..))")
+    public void detectCatCreate() {
+        Out.p("detectCatCreate");
+    }
+//    
+//    @AfterReturning("execution(** com.ray.world.factory.Factory.create(..))")
+//    public void recordCatCreate() {
+//        Out.p("recordCatCreate");
+//    }
+//    
+//    @AfterThrowing("execution(** com.ray.world.factory.Factory.create(..))")
+//    public void noRecordCatCreate() {
+//        Out.p("noRecordCatCreate");
+//    }
+//    
 }
